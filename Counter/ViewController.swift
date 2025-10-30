4aa54ab70715442b7c48d79d381364bdcaacd08c
balovo <arrowgr@yandex.ru>
2025-10-30 22:30:14 +0300

    Создан проект

---
+++ Counter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Counter
+//
+//  Created by Yuriy Mur on 30.10.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
